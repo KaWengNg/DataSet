1b6857e3a0d683d06dbaa0885407749de0b6de82
KaWengNg <104294867+KaWengNg@users.noreply.github.com>
2024-09-02 19:09:06 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -107,7 +107,7 @@ peft_config = LoraConfig(task_type="SEQ_CLS",
                         r=16,
                         lora_alpha=32,
                         bias="none",
-                        lora_dropout=0.2,
+                        lora_dropout=0.5,
                         target_modules=['query', "key"])
 
 model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=len(label2id), id2label=id2label, label2id=label2id)
@@ -116,26 +116,27 @@ model.print_trainable_parameters()
 
 # Training arguments
 batch_size = 8
-num_train_epochs = 12
+num_train_epochs = 70
 
 total_steps = len(train_dataset) // batch_size * num_train_epochs
-warmup_steps = int(0.1 * total_steps)
+warmup_steps = int(0.50 * total_steps)
+#print(warmup_steps)
 
 training_args = TrainingArguments(
     output_dir=model_checkpoint + "-lora-finetune",
-    learning_rate=5e-5,
+    learning_rate=1e-4,
     per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
     num_train_epochs=num_train_epochs,
-    # warmup_steps=warmup_steps,
-    weight_decay=0.01,
+    warmup_steps=warmup_steps,
+    weight_decay=0.1,
     evaluation_strategy="epoch",
     save_strategy="epoch",   
     logging_dir='./logs',
     logging_steps=10,
     save_total_limit=2,
     load_best_model_at_end=True,
-    lr_scheduler_type="cosine"
+    lr_scheduler_type="linear"
 )
 
 # Create Trainer
